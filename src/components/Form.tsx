bfd30a1e3f18425c54da452b10962681315572c2
Pedro L <pedrocontact22@gmail.com>
2022-09-26 17:26:39 -0300

    Added more colors + input focuses + forgot password link + hr

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,20 +6,24 @@ export default function Form() {
 					<input
 						type="text"
 						placeholder="Email or phone number"
-						className="flex mx-auto py-3 pl-4 pr-40 my-4 border border-gray-300 rounded-md"
+						className="flex mx-auto py-3 pl-4 pr-40 my-4 border border-gray-300 rounded-md focus:outline-none focus:border-primary focus:shadow-sm focus:placeholder:text-gray-300"
 					/>
 					<input
 						type="text"
 						placeholder="Password"
-						className="flex mx-auto py-3 pl-4 pr-40 my-4 border border-gray-300 rounded-md"
+						className="flex mx-auto py-3 pl-4 pr-40 my-4 border border-gray-300 rounded-md focus:outline-none focus:border-primary focus:shadow-sm focus:placeholder:text-gray-300"
 					/>
 					<button
 						type="submit"
-						className="flex mx-auto py-3 px-[9.5rem] my-4 rounded-md bg-primary text-xl text-white font-medium"
+						className="flex mx-auto py-3 px-[9.5rem] my-4 rounded-md bg-primary text-xl text-white font-medium hover:bg-primaryhover transition-all duration-100 ease-in"
 					>
 						Log In
 					</button>
 				</form>
+				<p className="text-center text-primary hover:underline cursor-pointer">
+					Forgot password?
+				</p>
+				<hr className="w-auto mx-3 my-3" />
 			</div>
 		</div>
 	);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       colors: {
         primary: '#1877f2',
-        secondary: '#42b72a'
+        primaryhover: '#1670e7',
+        secondary: '#42b72a',
+        secondaryhover: '#37a621'
       }
     },
     plugins: []
