994b8ee8e3f5f2252360e0cb75d57fb7c6a8ab34
Pedro L <pedrocontact22@gmail.com>
2022-09-26 16:27:52 -0300

    Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Form from "./components/Form";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App max-w-5xl">
+			<Form />
+		</div>
+	);
 }
 
 export default App;
