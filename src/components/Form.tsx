83869dba41b1091352fb779974d4a32daf860309
Pedro L <pedrocontact22@gmail.com>
2022-09-26 17:05:30 -0300

    Form + Submit Button + Primary and Secondary colors

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,26 @@
-import React from "react";
-
 export default function Form() {
-	return <div>Form</div>;
+	return (
+		<div>
+			<div className="shadow-lg sm:max-w-sm pb-12">
+				<form action="">
+					<input
+						type="text"
+						placeholder="Email or phone number"
+						className="flex mx-auto py-3 pl-4 pr-40 my-4 border border-gray-300 rounded-md"
+					/>
+					<input
+						type="text"
+						placeholder="Password"
+						className="flex mx-auto py-3 pl-4 pr-40 my-4 border border-gray-300 rounded-md"
+					/>
+					<button
+						type="submit"
+						className="flex mx-auto py-3 px-[9.5rem] my-4 rounded-md bg-primary text-xl text-white font-medium"
+					>
+						Log In
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'
-],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        primary: '#1877f2',
+        secondary: '#42b72a'
+      }
+    },
+    plugins: []
+  }
 }
